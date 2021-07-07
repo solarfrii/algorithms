a2f478303343a6b666bfc9c360972163a130c471
Dmitry <solfrii@ya.ru>
2021-07-07 20:31:26 +0300

    🏎

---
+++ fast_inverse_square_root/main.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
+
+
+int main()
+{
+  float number = 2;
+  float result = Q_rsqrt(number);
+  printf("Q_rsqrt(%f) = %f\n", number, result);
+  return 0;
+}
